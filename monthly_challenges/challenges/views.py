71ab9bfc8dbd20b843a9232444c75dd69f5917b0
Pedro <pmapm@ua.pt>
2021-09-02 20:10:51 +0100

    dynamic path segments

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse 
+from django.http import HttpResponse, HttpResponseNotFound 
 
 # Create your views here.
 
 # first view: takes a request and sents back a response
-def index(request):
+def january(request):
     return HttpResponse("This works!")
+
+def february(request):
+    return HttpResponse("Walk for at least 20min!")
+
+
+def monthly_challenge(request, month):
+    challenge_text = None
+    if month == 'january':
+        challenge_text = 'Eat no meat for the entire month'
+    elif month == 'february':
+        challenge_text = 'Walk for 20min'
+    elif month == 'march':
+        challenge_text = 'Learn django'
+    else:
+        return HttpResponseNotFound('This month is not suported!')
+    return HttpResponse(challenge_text)
