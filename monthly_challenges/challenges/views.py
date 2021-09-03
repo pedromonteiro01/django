141f3053f509f12da6f7358cb3768cd34a254b6c
Pedro <pmapm@ua.pt>
2021-09-03 11:23:27 +0100

    returning html instead of only plain text

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -33,6 +33,7 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = challenges[month]
-        return HttpResponse(challenge_text)
+        response_data = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_data)
     except:
-        return HttpResponseNotFound('This month is not suported!')
+        return HttpResponseNotFound('<h1>This month is not suported!</h1>')
