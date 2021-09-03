90ff03d014ba3b7baed7eb2cceddaeab8861b5fc
Pedro <pmapm@ua.pt>
2021-09-03 18:35:02 +0100

    if tag

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -14,7 +14,7 @@ challenges = {
     "september":"september challenge",
     "october":"october challenge",
     "november":"november challenge",
-    "december":"december challenge",
+    "december":None,
 }
 
 # Create your views here.
