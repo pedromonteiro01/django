83123a8f17336051b0d52f9fa0fb7a62253a2398
Pedro <pmapm@ua.pt>
2021-09-08 20:24:58 +0100

    changed shell output

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField()
+    
+def __str__(self): # how instances should be outputted
+    return f"{self.title} ({self.rating})"
