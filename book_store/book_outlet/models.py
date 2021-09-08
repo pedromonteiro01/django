4359fc480a514892d68428ddf4c0846d4893d49b
Pedro <pmapm@ua.pt>
2021-09-08 20:55:27 +0100

    changed data in the database

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db.models.fields import BooleanField
 
 # Create your models here.
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
-    rating = models.IntegerField()
+    rating = models.IntegerField(
+            validators=[MinValueValidator(1), MaxValueValidator(5)])
+    author = models.CharField(null=True, max_length=100) # if already created a table, and then add this field,  
+                                                        # this value can be null
+    is_bestselling = models.BooleanField(default=False)
     
 def __str__(self): # how instances should be outputted
     return f"{self.title} ({self.rating})"
