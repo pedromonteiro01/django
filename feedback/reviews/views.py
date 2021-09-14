e73acbf7305b8e9bd671692caa9f5c58cdcccfa2
Pedro <pmapm@ua.pt>
2021-09-14 12:25:46 +0100

    django forms and form controls

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
+from .forms import ReviewForm
+
 # Create your views here.
 
 def review(request):
     if request.method == 'POST':
-        entered_username = request.POST['username'] # {name:value}
-        print(entered_username)
-        return HttpResponseRedirect("/thank-you")
+        #entered_username = request.POST['username'] # {name:value}
+        form = ReviewForm(request.POST)
         
-    return render(request, "reviews/review.html")
+        if form.is_valid():
+            print(form.cleaned_data) # {formKey: value}
+            return HttpResponseRedirect("/thank-you")
+    else:
+        form = ReviewForm()
+
+    return render(request, "reviews/review.html", {
+        "form":form
+    })
 
 def thank_you(request):
     return render(request, "reviews/thank_you.html")
