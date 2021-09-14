30e8d03dafc4521862b37cd2faa8e8f2ed5a43bb
Pedro <pmapm@ua.pt>
2021-09-14 14:55:56 +0100

    ModelForms (configuring and saving data)

--- feedback/reviews/forms.py
+++ feedback/reviews/forms.py
@@ -1,5 +1,9 @@
 from django import forms
+from django.forms import fields
 
+from .models import Review
+
+'''
 class ReviewForm(forms.Form):
     user_name = forms.CharField(label="Your Name", max_length=100, error_messages={
         "required":"Your name must not be empty!",
@@ -7,3 +11,21 @@ class ReviewForm(forms.Form):
     })
     review_text = forms.CharField(label="Your Feedback", widget=forms.Textarea, max_length=200)
     rating = forms.IntegerField(label="Your Rating", min_value=1, max_value=5)
+'''
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = '__all__' # all fields, instead of giving a list
+        # exclude = ['owner_comment']
+        labels = {
+            'user_name':'Your Name',
+            'feedback':'Your Feedback',
+            'rating':'Your Rating'
+        }
+        error_messages = {
+            "user_name":{
+                "required":"Your name must not be empty!",
+                "max_length":"Please enter a shorter name!"
+            }
+        }
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -2,21 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
 from .forms import ReviewForm
-from .models import Review
+
 
 # Create your views here.
 
 def review(request):
     if request.method == 'POST':
         #entered_username = request.POST['username'] # {name:value}
-        form = ReviewForm(request.POST)
+        form = ReviewForm(request.POST) 
         
         if form.is_valid():
-            review = Review(
+            '''review = Review(
                 user_name=form.cleaned_data['user_name'], 
                 feedback = form.cleaned_data['review_text'], 
                 rating = form.cleaned_data['rating'])
-            review.save()
+            review.save()'''
+            form.save()
             return HttpResponseRedirect("/thank-you")
     else:
         form = ReviewForm()
