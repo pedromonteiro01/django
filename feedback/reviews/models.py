63c8b3c4c083d38d7559ee28350fc3da7341237f
Pedro <pmapm@ua.pt>
2021-09-14 14:27:40 +0100

    storing form data in a database

--- feedback/reviews/models.py
+++ feedback/reviews/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.forms.fields import CharField
 
 # Create your models here.
+
+class Review(models.Model):
+    user_name = models.CharField(max_length=100)
+    feedback = models.TextField()
+    rating = models.IntegerField()
