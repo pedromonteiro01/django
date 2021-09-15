aff7bc20264bf7f264a9a3f82bb427a6cb2761cd
Pedro <pmapm@ua.pt>
2021-09-15 14:16:02 +0100

    template view

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views import View
+from django.views.generic.base import TemplateView
 
 from .forms import ReviewForm
 
@@ -42,5 +43,15 @@ def review(request):
     })
 '''
 
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank-you.html"
+    
+    def get_context_data(self, **kwargs):
+        context =  super().get_context_data(**kwargs)
+        context['message'] = "This works!"
+        return context
+
+'''
 def thank_you(request):
     return render(request, "reviews/thank_you.html")
+'''
