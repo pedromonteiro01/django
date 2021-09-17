916013ddd95866aca7c069830f2c96b0eacbeebb
Pedro <pmapm@ua.pt>
2021-09-17 12:16:12 +0100

    using CreateView

--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
+from django.views.generic.edit import CreateView
 
 from .forms import ProfileForm
 from .models import UserProfile
@@ -12,23 +13,9 @@ from .models import UserProfile
     with open("temp/image.jpg", "wb+") as dest:
         for chunk in file.chunks():
             dest.write(chunk)'''
-
-class CreateProfileView(View):
-    def get(self, request):
-        form = ProfileForm()
-        return render(request, "profiles/create_profile.html", {
-            "form":form
-        })
-
-    def post(self, request): #when submit the form
-        #handle the upload file
-        submitted_form = ProfileForm(request.POST, request.FILES)
-        
-        if submitted_form.is_valid():   
-            profile = UserProfile(image=request.FILES['user_image'])
-            profile.save()
-            return HttpResponseRedirect("/profiles")
-        
-        return render(request, "profiles/create_profile.html", {
-            "form": submitted_form
-        })
+            
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile.html"
+    model = UserProfile
+    fields = "__all__"
+    success_url = "/profiles"
