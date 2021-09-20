b89af106ca6733172752ca0ed61e65ebb158adba
Pedro <pmapm@ua.pt>
2021-09-20 14:13:58 +0100

    styling comments and COmment Admin

--- project/3rd_version/my_site/blog/models.py
+++ project/3rd_version/my_site/blog/models.py
@@ -28,6 +28,9 @@ class Post(models.Model):
     author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name="posts")
     tags = ManyToManyField(Tag)
     
+    def __str__(self):
+        return self.title
+    
 class Comment(models.Model):
     user_name = models.CharField(max_length=100)
     user_email = models.EmailField()
